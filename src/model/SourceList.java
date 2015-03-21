5e7287fbe3129f5c0bb95356b400bff9e54b9a5c
33kou <okke.3wa6ra@ezweb.ne.jp>
2015-03-21 22:16:26 +0900

    wordとsource

--- src/model/SourceList.java
+++ src/model/SourceList.java
@@ -4,5 +4,25 @@ import java.util.ArrayList;
 
 public class SourceList {
     ArrayList<String> sourceList;
+    private ArrayList<String> Sources = new ArrayList<String>();
+    public ArrayList getAll()
+    {
+    	return Sources;
+    }
+    public void add(String adname)
+    {
+	    Sources.add(adname);
+    }
+    public void remove(String rmname)
+    {
+    	for (int i = 0 ; i < Sources.size() ; i++)
+    	{
+    	     if(rmname.equals((Sources.get(i)))) //renameとi番のSourcesの中身が一致すれば
+    	     {
+    	    	 Sources.remove(i);//i番をリストから消す
+    	     }
+    	}
 
+	
+    }
 }
--- src/model/WordList.java
+++ src/model/WordList.java
@@ -4,4 +4,24 @@ import java.util.ArrayList;
 
 public class WordList {
     ArrayList<String> wordList;
+    private ArrayList<String> words = new ArrayList<String>();
+    public ArrayList getAll()
+    {
+    	return words;
+    }
+    public void add(String adname)
+    {
+    	words.add(adname);
+    }
+    public void remove(String rename)
+    {
+    	for (int i = 0 ; i < words.size() ; i++)
+    	{
+    		if(rename.equals((words.get(i))))//renameとi番のwordsの中身が一致すれば
+    		{
+    			words.remove(i);//i番をリストから消す
+    		
+    		}
+    	}
+    }
 }
