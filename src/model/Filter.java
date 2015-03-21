c6c72342df5005cce579d6d97c46b3420799b986
Denshin <Denshin>
2015-03-21 20:27:31 +0900

    Filterクラスのスケルトンメソッド追加

--- src/model/Filter.java
+++ src/model/Filter.java
@@ -2,4 +2,44 @@ package model;
 
 public class Filter {
     String filterName;
+    WordList wordList;
+    SourceList sourceList;
+    NewsList newsList;
+
+
+    public WordList getWordList(){
+        return wordList;
+    }
+
+    public SourceList getSourceList(){
+        return sourceList;
+    }
+
+    public NewsList getNewsList(){
+        return newsList;
+    }
+
+    public String getFilterName(){
+        return filterName;
+    }
+
+    public void setWordList(WordList a){
+        wordList = a;
+    }
+
+    public void setSourceList(SourceList a){
+        sourceList = a;
+    }
+
+    public void setNewsList(NewsList a){
+        newsList = a;
+    }
+
+    public void setFilterName(String a){
+        filterName = a;
+    }
+
+    public void getUserNewsFromInternet(){
+
+    }
 }
